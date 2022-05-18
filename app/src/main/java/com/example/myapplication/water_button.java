f0122651806511d09dc1b5450ffb5bd60c35c725
Alexey Gushchin <ale20100039@gmail.com>
2022-05-18 23:44:22 +0500

    Done

---
+++ app/src/main/java/com/example/myapplication/water_button.java
@@ -0,0 +1,10 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class water_button {
+
+}
